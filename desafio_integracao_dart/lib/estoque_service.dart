0b24729aaa882036883e1ce2335657c712f8820e
Davifxm <davi1103felix@gmail.com>
2026-08-20 12:17:10 -0300

    Add files via upload

---
+++ desafio_integracao_dart/lib/estoque_service.dart
@@ -0,0 +1,25 @@
+class EstoqueService {
+  final Map<String, int> licencas = {};
+
+  void adicionarLicencas(String jogoId, int quantidade) {
+    licencas[jogoId] = quantidade;
+  }
+
+  bool possuiLicenca(String jogoId) {
+    return (licencas[jogoId] ?? 0) > 0;
+  }
+
+  void consumirLicenca(String jogoId) {
+    final quantidade = licencas[jogoId] ?? 0;
+
+    if (quantidade <= 0) {
+      return;
+    }
+
+    licencas[jogoId] = quantidade - 1;
+  }
+
+  int quantidade(String jogoId) {
+    return licencas[jogoId] ?? 0;
+  }
+}
---
+++ desafio_integracao_dart/lib/jogo.dart
@@ -0,0 +1,9 @@
+class Jogo {
+  final String id;
+  final String titulo;
+  final double preco;
+
+  Jogo(this.id, this.titulo, this.preco);
+
+  bool get gratuito => preco <= 0;
+}
